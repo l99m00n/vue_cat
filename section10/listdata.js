f4541ae77639354da96d2156026402e4a5aa30f5
lmnoon <tak.hat020712@gmail.com>
2019-04-30 13:38:12 +0900

    キーを使った要素の更新

--- section10/listdata.js
+++ section10/listdata.js
@@ -4,7 +4,7 @@ new Vue({
         list: [
             { id: 1, name: "スライム", hp: 100 },
             { id: 2, name: "ゴブリン", hp: 200 },
-            { id: 3, name: "ドラゴン", hp: 300 },
+            { id: 3, name: "ドラゴン", hp: 500 },
         ]
     }
 })
