9cee6530158babc513e429cfdebd95b77312fd33
lmnoon <tak.hat020712@gmail.com>
2019-04-30 14:18:38 +0900

    要素の追加と削除

--- section10/listdata.js
+++ section10/listdata.js
@@ -6,5 +6,25 @@ new Vue({
             { id: 2, name: "ゴブリン", hp: 200 },
             { id: 3, name: "ドラゴン", hp: 500 },
         ]
+    },
+    methods: {
+        // 追加ボタンをクリックしたときのハンドラ
+        doAdd: function() {
+            // リスト内で一番大きいIDを取得
+            var max = this.list.reduce(function(a, b) {
+                return a > b.id ? a : b.id
+            }, 0)
+            // 新しいモンスターをリストに追加
+            this.list.push({
+                id: max + 1,
+                name: this.name,
+                hp: 500,
+            })
+        },
+        // 削除ボタンをクリックしたときのハンドラ
+        doRemove: function(index) {
+            // 受け取ったインデックスの位置から1個要素を削除
+            this.list.splice(index, 1)
+        }
     }
 })
