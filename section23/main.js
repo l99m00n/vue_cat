8fb0b223122423d5081de80a35849ff663f603ee
lmnoon <tak.hat020712@gmail.com>
2019-05-29 23:08:14 +0900

    親から子へのデータの受け渡し

--- section23/main.js
+++ section23/main.js
@@ -1,12 +1,29 @@
+// Vue.component('comp-child', {
+//     // テンプレートでvalを使う
+//     template: '<p>{{ val }}</p>',
+//     // 受け取る属性を指定
+//     props: ['val'],
+// })
 Vue.component('comp-child', {
-    // テンプレートでvalを使う
-    template: '<p>{{ val }}</p>',
-    // 受け取る属性を指定
-    props: ['val'],
+    // v-forを用いて繰り返し表示する
+    template: '<li>{{ name }} HP: {{ hp }}\
+        <button v-on:click="doAttack">攻撃する</button></li>',
+    props: ['name', 'hp'],
+    methods: {
+        doAttack: function() {
+            // 親コンポーネントのデータをそのまま操作することになり警告がでる
+            this.hp -= 10
+        }
+    }
 })
 new Vue({
     el: "#app",
     data: {
         message: "Hello",
+        list: [
+            { id: 1, name: 'スライム', hp: 100 },
+            { id: 2, name: 'ゴブリン', hp: 300 },
+            { id: 3, name: 'ドラゴン', hp: 500 },
+        ],
     }
 })
